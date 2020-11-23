ccfdb22f90b4594d82327fbd16aaed94dae5d333
Oğuz Kayra Büyükyıldırım <74321096+Devy-Kayra@users.noreply.github.com>
2020-11-23 12:27:14 +0300

    Create Argument_Plot.R

---
+++ Argument_Plot.R
@@ -0,0 +1,9 @@
+x <- c(1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10)
+y <- c(10 , 25 , 35 , 45 , 55 , 50 , 40 , 30 , 20 , 20)
+
+plot(x, y , col = c("Red" , "Black") , type = "o" , lwd = 3 , pch = 1)
+
+# Col = color argument
+# Type = Type of plot
+# Lwd = line width
+# pch = marker style
