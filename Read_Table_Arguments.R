11d37bc652866eb85e736a5cbee55bce9020c22e
Oğuz Kayra Büyükyıldırım <74321096+Devy-Kayra@users.noreply.github.com>
2020-11-23 12:26:46 +0300

    Create Read_Table_Arguments.R

---
+++ Read_Table_Arguments.R
@@ -0,0 +1,7 @@
+table = read.table(file = 'xountries.txt' , sep = "," , nrow = 50 , header = TRUE)
+print(table)    
+
+# Header = create column name from top
+# Sep = select Seperate column character
+# nrow = limit row
+# File = File Path
