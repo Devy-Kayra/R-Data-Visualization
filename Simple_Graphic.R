22cc88d584ed80de037c6954fc6dca8d7ceebb72
Oğuz Kayra Büyükyıldırım <74321096+Devy-Kayra@users.noreply.github.com>
2020-11-23 12:29:16 +0300

    Create Simple_Graphic.R

---
+++ Simple_Graphic.R
@@ -0,0 +1,4 @@
+x <- c(1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10) # x axis vector
+y <- c(10 , 25 , 35 , 45 , 55 , 50 , 40 , 30 , 20 , 20) # y axis vector
+
+plot(x, y) # creating plot with x and y axis.
