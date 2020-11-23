b5135e315e14b78aa8ceb181795d3cae9ab0efe2
Oğuz Kayra Büyükyıldırım <74321096+Devy-Kayra@users.noreply.github.com>
2020-11-23 12:28:24 +0300

    Create New_Line.R

---
+++ New_Line.R
@@ -0,0 +1,6 @@
+x <- c(1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10) # X axis
+y <- c(10 , 25 , 35 , 45 , 55 , 50 , 40 , 30 , 20 , 20) # Y axis
+z <- c(50 , 35 , 45 , 55 , 15 , 20 , 30 , 12 , 30 , 40) # New Line Vector
+
+plot(x, y) #plot
+lines(z) # add new line function
